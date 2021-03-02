7cf8cb2ea950c2cf902799d81af26eb5f9ee2f75
Ian Davies <mr.daviesian@gmail.com>
2021-03-02 17:33:37 -0600

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,44 @@
 class BankAccount
 
+    attr_accessor :balance, :status, :deposit, :valid, :withdraw
+    attr_reader :name 
+
+    @@all = []
+
+    def initialize(name, balance=1000, status="open")
+        @name = name
+        @balance = balance
+        @status = status
+
+        @@all << self
+    end
+
+    def deposit(deposit)
+        @balance += deposit
+    end
+
+    def withdraw(withdraw)
+        @balance -= withdraw
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        if self.balance > 0 && self.status == "open"
+            true
+        else 
+            false
+        end
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+
+    def self.all
+        @@all
+    end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,43 @@
 class Transfer
-  # your code here
+
+  attr_accessor :amount, :status, :sender, :receiver
+  attr_reader 
+
+  def initialize(sender, receiver, amount, status="pending")
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+  end
+
+  def valid?
+    if sender.valid? && receiver.valid? == true
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction    
+    if receiver.valid? == true && sender.balance > amount && @status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      @status = "complete"
+    elsif sender.balance < amount
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    elsif receiver.valid? == false
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      @status = "reversed"
+    end
+  end
+
 end
